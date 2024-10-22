398fbf4d82d0a157c0ccb1e79e168006c9a59640
Carlos Oliveira <ramalhogithub@gmail.com>
2024-10-21 21:03:58 -0300

    Commit primário do projeto

---
+++ src/test/java/br/ce/wcaquino/tasksbackend/tasksbackend/TasksBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ce.wcaquino.tasksbackend.tasksbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TasksBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
